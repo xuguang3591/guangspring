44af87605ccefc9e99ba4e30ae277e90061aabea
ccc <511143221@qq.com>
2022-06-27 14:35:32 +0800

    first commit

---
+++ src/main/java/com/zhang/guangspring/GuangspringApplication.java
@@ -0,0 +1,13 @@
+package com.zhang.guangspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuangspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuangspringApplication.class, args);
+    }
+
+}
